5af28a1dd4a001a7d2af46eb0ec9eb300253b1e3
Yash Tripathi <yashdatttripathi@gmail.com>
2025-03-28 16:22:32 +0530

    emails fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,12 +173,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/yashtripathi08',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/yashtripathi7',
     }
 ];
 
